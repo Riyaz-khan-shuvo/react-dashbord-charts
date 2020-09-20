861ba55e4d29279c07a6a754e5a68808ff29c075
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2020-09-20 08:28:02 +0600

    react chart practice

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Chart from './Components/Chart/Chart';
+
+function App() {
+  return (
+    <div className="App">
+      <Chart></Chart>
+    </div>
+  );
+}
+
+export default App;
